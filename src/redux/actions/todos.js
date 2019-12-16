c83ac902a819486e5008e1daf71c2ede1ab62bcf
Felipe Romao <feliperomao.a@gmail.com>
2019-12-16 10:56:11 -0200

    consts redux

--- src/redux/actions/todos.js
+++ src/redux/actions/todos.js
@@ -1,6 +1,10 @@
+export const TODO_LIST = Object.freeze({
+  ASYNC_ADD_TODO: "todo/add_todo"
+});
+
 export function addTodo(text) {
   return {
-    type: "ASYNC_ADD_TODO",
+    type: TODO_LIST.ASYNC_ADD_TODO,
     text
   };
 }
--- src/redux/saga/sagas.js
+++ src/redux/saga/sagas.js
@@ -2,10 +2,10 @@ import { takeLatest, put, all, delay } from "redux-saga/effects";
 
 function* asyncAddTodo(action) {
   yield delay(1000);
-
+  
   yield put({ type: "ADD_TODO", text: action.text });
 }
 
 export default function* root() {
-  yield all([takeLatest("ASYNC_ADD_TODO", asyncAddTodo)]);
+  yield all([takeLatest("todo/add_todo", asyncAddTodo)]);
 }
